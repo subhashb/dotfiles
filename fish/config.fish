b354512dec7e0099038e5e4b1c29d5d5e325498b
Subhash Bhushan <subhash.bhushan@kaybus.com>
2015-01-28 08:20:23 +0530

    Add emacs and rbenv config

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,17 @@ set -g -x PATH /usr/local/bin $PATH
 
 # Paths
 # While you can use a single fish array to do this (space-separated list), this is easier to read
-set PATH /usr/local/bin /usr/local/sbin $HOME/.bin $HOME/.rvm/bin $PATH
+set PATH ~/.rbenv/bin /usr/local/bin /usr/local/sbin $HOME/.bin $PATH
+
+# rbenv configuration
+set -gx RBENV_ROOT ~/.rbenv
+. (rbenv init -|psub)
+
+# Emacsclient Config
+set EDITOR emacsclient -c
+set ALTERNATE_EDITOR ""
+set VISUAL $EDITOR
+alias ec "emacsclient -c -n"
 
 alias gs "git status"
 alias gd "git diff"
@@ -10,5 +20,8 @@ alias gp "git push origin "
 alias gu "git pull origin "
 alias ga "git add ."
 alias gc "git commit"
+alias gch "git checkout"
+alias gf "git fetch"
 
 alias b "bundle"
+
